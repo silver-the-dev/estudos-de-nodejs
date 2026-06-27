e24e13b313c14567bb62d6884a3b42f324613236
SilverDev <joaoosilverio2004@hotmail.com>
2026-06-26 23:59:25 -0300

    3. Filtros dinâmicos

--- ex03/index.js
+++ ex03/index.js
@@ -1,7 +1,17 @@
 import e from "express";
 const app = e();
 const PORT = 3000;
+app.use(e.json());
 
-app.listen(3000, () => {
-    console.log(`Port ${PORT}`);
-});
+app.get('/calculator', (req, res) => {
+    const num1 = Number(req.query.num1);
+    const num2 = Number(req.query.num2);
+
+    const sum = num1 + num2;
+
+    res.send({ 
+        message: `${sum}`
+    });
+})
+
+app.listen(3000, () => console.log(`Port ${PORT}`));
